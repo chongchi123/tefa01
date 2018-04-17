ae4dd208be1ca11097d31dd244c10b18a6840d49
zhu <407106121@qq.com>
2018-04-17 05:52:48 -0700

    123

---
+++ tefa01.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/home/')
+def home():
+    return "哈哈哈"
+
+
+if __name__ == '__main__':
+    app.run()
